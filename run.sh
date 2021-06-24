b1bb58e23cebb31fe9b1a35cf2e91b43f75232ac
HAOYUatHZ <haoyu@protonmail.com>
2021-06-24 18:49:33 +0800

    add coordinator configuration

--- run.sh
+++ run.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 set -uex
 
+# TODO: detect file and skip
+
 export NTXS=2;
 export BALANCELEVELS=3;
 export ORDERLEVELS=4;
@@ -9,17 +11,33 @@ export VERBOSE=false;
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 STATE_MNGR_DIR=$DIR/rollup-state-manager
-CIRCUITS_REPO_DIR=$STATE_MNGR_DIR/circuits
-TARGET_CIRCUIT_DIR=$CIRCUITS_REPO_DIR/testdata/Block_$NTXS"_"$BALANCELEVELS"_"$ORDERLEVELS"_"$ACCOUNTLEVELS
+CIRCUITS_DIR=$STATE_MNGR_DIR/circuits
+TARGET_CIRCUIT_DIR=$CIRCUITS_DIR/testdata/Block_$NTXS"_"$BALANCELEVELS"_"$ORDERLEVELS"_"$ACCOUNTLEVELS
+PROVER_DIR=$DIR/prover-cluster
 
 # make sure submodule is correctly cloned!!
 git submodule update --init --recursive
 if [ -z ${CI+x} ]; then git pull --recurse-submodules; fi
 
+rm $TARGET_CIRCUIT_DIR -rf
 cd $STATE_MNGR_DIR
 cargo run --bin gen_export_circuit_testcase
 
-cd $CIRCUITS_REPO_DIR
+cd $CIRCUITS_DIR
 npm i
+# TODO: detect and install snarkit
 snarkit compile $TARGET_CIRCUIT_DIR --force_recompile --backend=native
 
+cd $PROVER_DIR
+cargo build --release
+
+PORT=50055
+DB_URL=postgres://coordinator:coordinator_AA9944@127.0.0.1/prover_cluster
+printf 'port: %d
+db: "%s"
+witgen:
+  interval: 10000
+  n_workers: 5
+  circuits:
+    block: "%s/circuit.fast"
+' $PORT $DB_URL $TARGET_CIRCUIT_DIR > $PROVER_DIR/config/coordinator.yaml
