4aad930d23ae0ae8fd7ae7823e709655cd330055
lightsing <light.tsing@gmail.com>
2021-09-16 15:52:01 +0800

    fix typo

--- run.sh
+++ run.sh
@@ -66,7 +66,7 @@ EOF
 
 function prepare_contracts() {
   rm -f $CONTRACTS_DIR/contracts/Verifier.sol
-  plonkit generate-verifier -v $TARGET_CIRCUIT_DIR/vk.bin -s $CONTRACTS_DIR/contracts/verifier.sol
+  plonkit generate-verifier -v $TARGET_CIRCUIT_DIR/vk.bin -s $CONTRACTS_DIR/contracts/Verifier.sol
   cd $CONTRACTS_DIR/
   git update-index --assume-unchanged $CONTRACTS_DIR/contracts/Verifier.sol
   yarn install
