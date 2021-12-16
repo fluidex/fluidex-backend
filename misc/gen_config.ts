56deec4861812eb5ae51581dafa657c05bcac639
lightsing <light.tsing@gmail.com>
2021-12-16 18:50:51 +0800

    fix typo

--- misc/gen_config.ts
+++ misc/gen_config.ts
@@ -5,7 +5,7 @@ import deployed from '/tmp/deployed.json';
 
 async function main() {
     const listener_template = fs.readFileSync('../eth_listener/config.toml.template', 'utf-8');
-    const NETWORK = process.env.NETWORK ?? "geth";
+    const NETWORK = process.env.DX_NETWORK ?? "geth";
     const INFRUA_API_KEY = secrets.infuraApiKey;
     const CONTRACT_ADDRESS = deployed.FluiDexDelegate;
     const INNER_CONTRACT_ADDRESS = deployed.FluiDexDemo;
