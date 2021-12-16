751dd39d9afd1e3e86245dc4c33e5eb495c7278f
lightsing <light.tsing@gmail.com>
2021-12-16 13:54:04 +0800

    add gen_config

---
+++ misc/gen_config.ts
@@ -0,0 +1,21 @@
+import * as fs from 'fs';
+import secrets from '../contracts/secrets.json';
+import tokens from '/tmp/tokens.json';
+import deployed from '/tmp/deployed.json';
+
+async function main() {
+    const listener_template = fs.readFileSync('../eth_listener/config.toml.template', 'utf-8');
+    const NETWORK = process.env.NETWORK ?? "geth";
+    const INFRUA_API_KEY = secrets.infuraApiKey;
+    const CONTRACT_ADDRESS = deployed.FluiDexDelegate;
+    const INNER_CONTRACT_ADDRESS = deployed.FluiDexDemo;
+    const BASE_BLOCK = deployed.baseBlock;
+    console.log(eval("`" + listener_template + "`"));
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch(error => {
+    console.error(error);
+    process.exit(1);
+  });
